9999c2d41d629de548d2ab3d0e7d8cb072dbff66
asaad14 <144741132+asaad14@users.noreply.github.com>
2023-10-07 17:20:47 +0100

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -3,13 +3,13 @@ $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-$email_from = 'gslemediateurschool@gmail.com';
+$email_from = 'assaadelhamiani12@gmail.com';
 $email_subjet = 'New Form Submission';
 $email_body = "User Name: $name.\n".
                "User Email: $visitor-email.\".
                 "Subject: $subject.\n".
                 "User Message: $message.\";
-$to ='assaadelhamiani12@gmail.com';
+$to ='gslemediateurschool@gmail.com';
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
